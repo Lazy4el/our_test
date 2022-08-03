67f5ab757f095eb5fae074f91ac11f4f204a69bd
ArtemMayorov <artmai.pg@outlook.com>
2022-08-03 19:49:54 +0300

    add pages and header component

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+
+const HomePage: FC = () => {
+    return (
+        <div>
+            Я Главная страница
+        </div>
+    )
+}
+export  default HomePage
---
+++ src/components/App/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import HomePage from "../../Pages/HomePage/HomePage";
+import Header from "../Header/Header";
+import ProductsPage from "../../Pages/ProductsPage/ProductsPage";
+import AboutPage from "../../Pages/AboutPage/AboutPage";
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/products' element={<ProductsPage/>}/>
+            <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
